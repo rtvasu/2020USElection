fc0bf628fdab1e1743867f48e707e0021b897e34
aishsr <aishwarya-sri99@hotmail.com>
2021-04-01 23:20:16 -0400

    Search queries and navigation done

--- CLI.py
+++ CLI.py
@@ -1,10 +1,7 @@
 import mysql.connector
 from mysql.connector import errorcode
 import locale
-import decimal
-import fire
 import getpass
-import sys
 from electionDB import electionDB
 
 
@@ -48,80 +45,245 @@ else:
     """
         MAIN
     """
-    print("............................Welcome to the 2020 election database system............................")
-    print("\n")
-
-
 
     while (1):
-        # FIND VOTES BY a particular state
-        print("---------------- States ----------------")
-        stateInput = input("Please enter any state. To get a full list of votes per state, press A: ")
 
-        totalVotes = results.totalVotesByState(stateInput)
-        if (not totalVotes):
-            print("Please enter a valid state")
-        else:
-            for i in totalVotes:
-                state = locale.format_string("%s", i[0], grouping=True)
-                votes = locale.format_string("%d", i[1], grouping=True)
-                print("The total votes in ", state, " is ", votes, " votes.")
+        print(
+            "------------------------------- Welcome to the 2020 election database system -------------------------------")
+        print("\n")
+
+
+        #Main Screen
+        print("What do you want to do?")
+        print("(S) Get statistics")
+        print("(I) Insert Data")
+        print("(D) Data Mine")
+        print("\n")
+
+        command = input ("Your command: ")
+        print("\n")
+
+        if (command == 's'):
+
+
+            while (1):
+
+                print("............................................................................................ ...")
+                print ("Welcome to search. These are the actions you can do. Please choose one of them: ")
+
+                print("(1) Voting results for a state")
+                print("(2) Voting results for all states")
+                print("(3) Voting results for a county")
+                print("(4) Voting results for all counties in a state")
+                print("(5) Get the most popular tweets from a state")
+                print("(6) Demographics of a state")
+                print("(7) Demographics of a county")
+                print("( ANY KEY ) Exit")
+                print("\n")
+                command_s = input("Your command: ")
+                print("\n")
+
+                if (command_s == '1'):
+
+                    print("(1) Voting results for a state")
+                    print("\n")
+                    stateInput = input("Please enter a state: ")
+                    print("\n")
+
+                    totalVotes = results.totalVotesByState(stateInput)
+                    if (not totalVotes):
+                        print("Please try again.")
+                        print("\n")
+                    else:
+                        for i in totalVotes:
+                            state = locale.format_string("%s", i[0], grouping=True)
+                            votes = locale.format_string("%d", i[1], grouping=True)
+                            print("The total votes in ", state, " is ", votes, " votes.")
+                        print("\n")
+
+
+                elif (command_s == '2'):
+
+                    print("(2) Voting results for all states")
+                    print("\n")
+
+                    totalVotes = results.totalVotesByState('A')
+                    if (not totalVotes):
+                        print("Please try again.")
+                        print("\n")
+                    else:
+                        for i in totalVotes:
+                            state = locale.format_string("%s", i[0], grouping=True)
+                            votes = locale.format_string("%d", i[1], grouping=True)
+                            print("The total votes in ", state, " is ", votes, " votes.")
+                        print("\n")
+
+
+
+                elif (command_s == '3'):
+                    print("(3) Voting results for a county")
+                    print("\n")
+                    askState = input("Which state? ")
+                    print("\n")
+                    stateList = results.getListioCountiesUnderState(askState)
+                    if (not stateList):
+                        print("Please try again.")
+                        print("\n")
+                    else:
+                        for i in stateList:
+                            print(i[0])
+                        print("\n")
+
+                        county = input(
+                            "Here is a list of counties in this state. Please choose a county to view results for: ")
+                        countyResults = results.totalVotesByCounty(askState, county)
+
+                        if (not countyResults):
+                            print("Please try again.")
+                            print("\n")
+                        else:
+                            for i in countyResults:
+                                votes = locale.format_string("%d", i[2], grouping=True)
+                                print("The total votes in ", county, " is ", votes, " votes.")
+                            print("\n")
+
+
+
+                elif (command_s == '4'):
+                    print("(4) Voting results for all counties in a state")
+                    print("\n")
+                    askState = input("Which state? ")
+                    print("\n")
+                    stateList = results.getListioCountiesUnderState(askState)
+                    if (not stateList):
+                        print("Please try again.")
+                        print("\n")
+                    else:
+                        countyResults = results.totalVotesByCounty(askState, 'A')
+
+                        if (not countyResults):
+                            print("Please try again.")
+                            print("\n")
+                        else:
+                            for i in countyResults:
+                                county = locale.format_string("%s", i[1], grouping=True)
+                                votes = locale.format_string("%d", i[2], grouping=True)
+                                print("The total votes in ", county, " is ", votes, " votes.")
+                            print("\n")
+
+
+
+
+
+                elif (command_s == '5'):
+                    print("(5) Get the most popular tweets from a state")
+                    print("\n")
+                    stateInput = input("Please enter a state: ")
+                    print("\n")
+                    numberOfTweets = input("How many tweets do you want to view? ")
+                    print("\n")
+
+                    bidenTweets = results.tweetsBiden(stateInput, numberOfTweets)
+                    if (not bidenTweets):
+                        print("Please try again.")
+                        print("\n")
+                    else:
+                        print ("Most popular tweets under #TweetsBiden for %s:" % stateInput)
+                        for i in bidenTweets:
+                            tweet = locale.format_string("%s", i[2], grouping=True)
+                            likes = locale.format_string("%d", i[3], grouping=True)
+                            retweets = locale.format_string("%d", i[4], grouping=True)
+                            print(" - " + tweet)
+                            print("(Likes: " + likes + ", Retweets: ", retweets + ")")
+                            print("\n")
+                        print("\n")
+
+                    print("--------------------------------------------------------------------------")
+
+                    trumpTweets = results.tweetsTrump(stateInput, numberOfTweets)
+                    if (not trumpTweets):
+                        print("Please try again.")
+                        print("\n")
+                    else:
+                        print("Most popular tweets under #TweetsTrump for %s:" % stateInput)
+                        for i in trumpTweets:
+                            tweet = locale.format_string("%s", i[2], grouping=True)
+                            likes = locale.format_string("%d", i[3], grouping=True)
+                            retweets = locale.format_string("%d", i[4], grouping=True)
+                            print(" - " + tweet)
+                            print("(Likes: " + likes + ", Retweets: ", retweets + ")")
+                            print("\n")
+                        print("\n")
+
+
+                elif (command_s == '6'):
+                    print("(6) Demographics of a state")
+                    print("\n")
+                    state = input("To search for a particular state, enter a state: ")
+                    print("\n")
+                    demographics = results.demographicsByState(state)
+
+                    if (not demographics):
+                        print("Please try again.")
+                        print("\n")
+                    else:
+                        for i in demographics:
+                            print("-- ", locale.format_string("%s", i[0], grouping=True), " Demographics --")
+                            print("Total Population - ", locale.format_string("%d", i[1], grouping=True))
+                            print("Men - ", locale.format_string("%s", i[2], grouping=True), "%")
+                            print("Women - ", locale.format_string("%s", i[3], grouping=True), "%")
+                            print("White - ", locale.format_string("%s", i[4], grouping=True), "%")
+                            print("Black - ", locale.format_string("%s", i[5], grouping=True), "%")
+                            print("Hispanic - ", locale.format_string("%s", i[6], grouping=True), "%")
+                            print("Asian - ", locale.format_string("%s", i[7], grouping=True), "%")
+                            print("Native - ", locale.format_string("%s", i[8], grouping=True), "%")
+                        print("\n")
+
+
+                elif (command_s == '7'):
+                    print("(7) Demographics of a county")
+                    print("\n")
+                    askState = input("Which state? ")
+                    stateList = results.getListioCountiesUnderState(askState)
+                    if (not stateList):
+                        print("Please try again.")
+                        print("\n")
+                    else:
+                        for i in stateList:
+                            print(i[0])
+                        print("\n")
+
+                        county = input(
+                            "Here is a list of counties in this state. Please choose a county to view results for: ")
+                        demographics = results.demographicsByCounty(askState, county)
+
+                        if (not demographics):
+                            print("Please try again.")
+                            print("\n")
+                        else:
+                            for i in demographics:
+                                print("-- ", locale.format_string("%s", i[1], grouping=True), " Demographics --")
+                                print("Total Population - ", locale.format_string("%d", i[2], grouping=True))
+                                print("Men - ", locale.format_string("%s", i[3], grouping=True), "%")
+                                print("Women - ", locale.format_string("%s", i[4], grouping=True), "%")
+                                print("White - ", locale.format_string("%s", i[5], grouping=True), "%")
+                                print("Black - ", locale.format_string("%s", i[6], grouping=True), "%")
+                                print("Hispanic - ", locale.format_string("%s", i[7], grouping=True), "%")
+                                print("Asian - ", locale.format_string("%s", i[8], grouping=True), "%")
+                                print("Native - ", locale.format_string("%s", i[9], grouping=True), "%")
+                            print("\n")
+                else:
+                    print("Thank you!")
+                    break
+
+        elif (command == 'i'):
+            print("Insertion coming soon")
             print("\n")
 
 
-
-
-
-
-
-        # FIND VOTES BY a particular county
-        print("---------------- Counties ----------------")
-        yesToCounties = input("Would you like to view the voting results for a particular county - Y/N?: ")
-
-        if (yesToCounties == 'y'):
-            askState = input("Which state? ")
-            stateList = results.getListioCountiesUnderState(askState)
-            if (not stateList):
-                print("Please enter a valid state.")
-            else:
-                for i in stateList:
-                    print(i[0])
-
-                county = input("Here is a list of counties in this state. Please choose a county to view results for: ")
-                countyResults = results.totalVotesByCounty(county)
-
-                if (not countyResults):
-                    print("Please enter a valid county.")
-                else:
-                    for i in countyResults:
-                        votes = locale.format_string("%d", i[1], grouping=True)
-                        print("The total votes in ", county, " is ", votes, " votes.")
-                    print("\n")
-
-
-
-
-
-
-        # FIND demographics for a county/state
-        print("---------------- Demographics ----------------")
-        state = input("Demographics. To search for a particular state, enter a state, press A: ")
-
-        demographics = results.demographicsByState(state)
-
-        if (not demographics):
-            print("Please enter a valid state.")
-        else:
-            for i in demographics:
-                print("-- ", locale.format_string("%s", i[0], grouping=True), " Demographics --")
-                print("Total Population - ", locale.format_string("%d", i[1], grouping=True))
-                print("White - ", locale.format_string("%d", i[2], grouping=True), "%")
-                print("Black - ", locale.format_string("%d", i[3], grouping=True), "%")
-                print("Hispanic - ", locale.format_string("%d", i[4], grouping=True), "%")
-                print("Asian - ", locale.format_string("%d", i[5], grouping=True), "%")
-                print("Native - ", locale.format_string("%d", i[6], grouping=True), "%")
-                print("Pacific - ", locale.format_string("%d", i[7], grouping=True), "%")
-                print("\n")
+        elif (command == 'd'):
+            print("Data Mining coming soon")
+            print("\n")
 
     # remember to close connection
     cursor.close()
--- electionDB.py
+++ electionDB.py
@@ -17,7 +17,10 @@ class electionDB:
 
         cursor = self.cursor
 
-        query = ("""SELECT name as countyName FROM County WHERE state = '%s'""" % state)
+        query = ("""SELECT c.name as countyName 
+                    FROM County c
+                    inner join States s on (c.stateid = s.stateid)
+                    WHERE s.name = '%s'""" % state)
         cursor.execute(query)
         result = cursor.fetchall()
         return result
@@ -27,43 +30,50 @@ class electionDB:
         cursor = self.cursor
         filterByStateCond = ""
         if (state != 'a' and state != 'A'):
-            filterByStateCond = "having c.state = '" + state + "'"
+            filterByStateCond = "having s.name = '" + state + "'"
 
-        query = ("""SELECT  c.state as state ,
+        query = ("""SELECT  s.name as state ,
                           sum(total_votes) as total_votes_2020
-                  FROM VotesPerCounty v
-                  inner join County c on (v.county = c.name)
-                  group by c.state
-                  %s
-                  order by c.state asc""" % filterByStateCond)
+                    FROM VotesPerCounty v
+                    inner join County c on (v.countyid = c.countyid)
+                    inner join States s on (c.stateid = s.stateid)
+                    group by s.name 
+                    %s
+                    order by s.name asc""" % filterByStateCond)
         cursor.execute(query)
         result = cursor.fetchall()
         return result
 
 
 
-    def totalVotesByCounty(self, county):
+    def totalVotesByCounty(self, state, county):
 
         cursor = self.cursor
-        query = ("""SELECT  c.name as county ,
-                          sum(total_votes) as total_votes_2020
-                  FROM VotesPerCounty v
-                  inner join County c on (v.county = c.name)
-                  group by c.name
-                  having c.name = '%s'
-                  order by c.name asc""" % county)
+        filterByCountyCond = ""
+        if (county != 'a' and county != 'A'):
+            filterByCountyCond = "and c.name = '" + county + "'"
+
+        query = ("""SELECT  s.name as state, c.name as county ,
+                        sum(total_votes) as total_votes_2020
+                    FROM VotesPerCounty v
+                    inner join County c on (v.countyid = c.countyid)
+                    inner join States s on (c.stateid = s.stateid)
+                    group by c.name, s.name
+                    having s.name = '%s' %s
+                     order by c.name asc""" % (state,filterByCountyCond))
         cursor.execute(query)
         result = cursor.fetchall()
         return result
+
+
 
     def demographicsByState(self, state):
 
         cursor = self.cursor
-        filterByStateCond = ""
-        if (state != 'a' and state != 'A'):
-            filterByStateCond = "cs.state = '" + state + "' and"
+        # if (state != 'a' and state != 'A'):
+        filterByStateCond = "ss.name = '" + state + "' and"
 
-        query = ("""select cs.state as 'States', 
+        query = ("""select ss.name as 'States', 
                          ROUND(sum(pop_per_county),2) as 'Total Population',
                          ROUND((sum(demographicMen)/sum(pop_per_county))*100,2) as 'Percentage of Men',
                          ROUND((sum(demographicWomen)/sum(pop_per_county))*100,2) as 'Percentage of Women',
@@ -71,29 +81,110 @@ class electionDB:
                          ROUND((sum(demographicBlack)/sum(pop_per_county))*100,2) as 'Percentage of Black',
                          ROUND((sum(demographicHispanic)/sum(pop_per_county))*100,2) as 'Percentage of Hispanic',
                          ROUND((sum(demographicAsian)/sum(pop_per_county))*100,2) as 'Percentage of Asian',
-                         ROUND((sum(demographicNative)/sum(pop_per_county))*100,2) as 'Percentage of Native',
-                         ROUND((sum(demographicPacific)/sum(pop_per_county))*100,2) as 'Percentage of Pacific'
+                         ROUND((sum(demographicNative)/sum(pop_per_county))*100,2) as 'Percentage of Native'
                      from (
                          SELECT  c.name as county, 
                          TotalPop as pop_per_county,
 
-                         (s.Men) as demographicMen,
-                         (s.Women) as demographicWomen,
-                         (s.White*TotalPop/100) as demographicWhite,
-                         (s.Black*TotalPop/100) as demographicBlack,
-                         (s.Hispanic*TotalPop/100) as demographicHispanic,
-                         (s.Asian*TotalPop/100) as demographicAsian,
-                         (s.Native*TotalPop/100) as demographicNative,
-                         (s.Pacific*TotalPop/100) as demographicPacific
+                         (cs.Men) as demographicMen,
+                         (cs.Women) as demographicWomen,
+                         (cs.White*TotalPop/100) as demographicWhite,
+                         (cs.Black*TotalPop/100) as demographicBlack,
+                         (cs.Hispanic*TotalPop/100) as demographicHispanic,
+                         (cs.Asian*TotalPop/100) as demographicAsian,
+                         (cs.Native*TotalPop/100) as demographicNative
 
-                         FROM CountyStats s
-                         inner join County c on (s.countyid = c.countyid)
+                         FROM CountyStats cs
+                         inner join County c on (cs.countyid = c.countyid)
+                         inner join States s on (c.stateid = s.stateid)
                      ) as Stats
 
-                     inner join County cs on (cs.name = county)
-                     group by cs.state
-                     having %s cs.state != 'Alaska'
-                     order by cs.state asc""" % filterByStateCond)
+                     inner join County co on (co.name = county)
+                     inner join States ss on (co.stateid = ss.stateid)
+                     group by ss.name 
+                     having %s ss.name != 'Alaska'
+                     order by ss.name asc""" % filterByStateCond)
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+
+
+
+
+
+    def demographicsByCounty(self, state, county):
+
+        cursor = self.cursor
+        # if (state != 'a' and state != 'A'):
+        filterByCountyCond = "and c.name = '" + county + "'"
+
+        query = ("""select ss.name as 'States', co.name as 'County', 
+                         ROUND(sum(pop_per_county),2) as 'Total Population',
+                         ROUND((sum(demographicMen)/sum(pop_per_county))*100,2) as 'Percentage of Men',
+                         ROUND((sum(demographicWomen)/sum(pop_per_county))*100,2) as 'Percentage of Women',
+                         ROUND((sum(demographicWhite)/sum(pop_per_county))*100,2) as 'Percentage of White',
+                         ROUND((sum(demographicBlack)/sum(pop_per_county))*100,2) as 'Percentage of Black',
+                         ROUND((sum(demographicHispanic)/sum(pop_per_county))*100,2) as 'Percentage of Hispanic',
+                         ROUND((sum(demographicAsian)/sum(pop_per_county))*100,2) as 'Percentage of Asian',
+                         ROUND((sum(demographicNative)/sum(pop_per_county))*100,2) as 'Percentage of Native'
+                     from (
+                         SELECT  c.name as county, 
+                         TotalPop as pop_per_county,
+
+                         (cs.Men) as demographicMen,
+                         (cs.Women) as demographicWomen,
+                         (cs.White*TotalPop/100) as demographicWhite,
+                         (cs.Black*TotalPop/100) as demographicBlack,
+                         (cs.Hispanic*TotalPop/100) as demographicHispanic,
+                         (cs.Asian*TotalPop/100) as demographicAsian,
+                         (cs.Native*TotalPop/100) as demographicNative
+
+                         FROM CountyStats cs
+                         inner join County c on (cs.countyid = c.countyid)
+                         inner join States s on (c.stateid = s.stateid)
+                     ) as Stats
+
+                     inner join County co on (co.name = county)
+                     inner join States ss on (co.stateid = ss.stateid)
+                     group by ss.name, co.name
+                     having ss.name = '%s' and co.name = '%s' and ss.name != 'Alaska'
+                     order by ss.name asc""" % (state, county))
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+
+
+    def tweetsBiden(self, state, number):
+
+        cursor = self.cursor
+
+        if (not number):
+            number = 5
+
+        query = ("""SELECT s.name, created_at, tweet, likes, retweets 
+                    FROM TweetsBiden t
+                    inner join States s on (t.stateid = s.stateid)
+                    WHERE s.name = '%s'
+                    ORDER BY t.likes DESC
+                    limit %s""" % (state, number))
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+
+
+    def tweetsTrump(self, state, number):
+
+        cursor = self.cursor
+
+        if (not number):
+            number = 5
+
+        query = ("""SELECT s.name, created_at, tweet, likes, retweets 
+                    FROM TweetsTrump t
+                    inner join States s on (t.stateid = s.stateid)
+                    WHERE s.name = '%s'
+                    ORDER BY t.likes DESC
+                    limit %s""" % (state, number))
         cursor.execute(query)
         result = cursor.fetchall()
         return result
